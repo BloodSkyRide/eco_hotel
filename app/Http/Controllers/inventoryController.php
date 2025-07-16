31cc9551c74ea9aa9f5f9b9a567c39a1ec06a802
santiago <santiago.gallego1@utp.edu.co>
2025-07-16 18:48:34 -0500

    solucion de backend error

--- app/Http/Controllers/inventoryController.php
+++ app/Http/Controllers/inventoryController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\modelInventario;
 use App\Models\modelCompuesto;
 use App\Models\modelcontrol_inventarios;
@@ -13,30 +14,31 @@ class inventoryController extends Controller
 
     private $category_kitchen = "cocina";
     private $category_store = "tienda";
-    
-    
 
-    public function getShowInventory(Request $request){
+
+
+    public function getShowInventory(Request $request)
+    {
 
         $token_header = $request->header("Authorization");
 
-        $replace = str_replace("Bearer ","",$token_header);
+        $replace = str_replace("Bearer ", "", $token_header);
 
         $decode_token = JWTAuth::setToken($replace)->authenticate();
 
         $rol = $decode_token["rol"];
 
-        $productos_inventario = ($rol == "administrador") ? modelInventario::getAllProducts(): modelInventario::getProductsCategory($this->category_store);
+        $productos_inventario = ($rol == "administrador") ? modelInventario::getAllProducts() : modelInventario::getProductsCategory($this->category_store);
         $total_inventory = modelInventario::getTotalInventory();
         $comparative = self::comparativeInventory(date("Y-m-d"));
 
-        $render = view("menuDashboard.inventory", ["rol" => $rol, "productos" => $productos_inventario,"total" => $total_inventory, "comparativo" => $comparative])->render();
+        $render = view("menuDashboard.inventory", ["rol" => $rol, "productos" => $productos_inventario, "total" => $total_inventory, "comparativo" => $comparative])->render();
 
         return response()->json(["status" => true, "html" => $render]);
-
     }
 
-    public function saveInventory(Request $request){
+    public function saveInventory(Request $request)
+    {
 
         $nombre_producto = $request->nombre_producto;
         $unidades = $request->unidades;
@@ -46,20 +48,29 @@ class inventoryController extends Controller
 
         $fecha = date("Y-m-d");
         $hour = date('h:i:s');
-        
-        $data_inventory = ["nombre" => $nombre_producto, "unidades_disponibles" => $unidades, "fecha_creacion" => $fecha, "hora_creacion" => $hour, "tope_min" => $tope_min,
-        "abastecimiento" => "ABASTECIDO", "precio_costo" => $precio_costo, "estado" => "DISPONIBLE", "categoria" => $categoria];
+
+        $data_inventory = [
+            "nombre" => $nombre_producto,
+            "unidades_disponibles" => $unidades,
+            "fecha_creacion" => $fecha,
+            "hora_creacion" => $hour,
+            "tope_min" => $tope_min,
+            "abastecimiento" => "ABASTECIDO",
+            "precio_costo" => $precio_costo,
+            "estado" => "DISPONIBLE",
+            "categoria" => $categoria
+        ];
 
         $insert_product_inventory = modelInventario::createProduct($data_inventory);
 
-        
-        if($insert_product_inventory) return self::getShowInventory($request);
+
+        if ($insert_product_inventory) return self::getShowInventory($request);
         else return response()->json(["status" => false]);
-        
     }
 
 
-    public function editInventory(Request $request){
+    public function editInventory(Request $request)
+    {
 
         $unidades = $request->unidades;
         $id_inventory = $request->id_inventory;
@@ -67,37 +78,32 @@ class inventoryController extends Controller
         $name_inventory = $request->nombre_inventario;
         $units_establishing = $request->units_establishing;
 
-    
-        if($units_establishing !== null){
-            
-            
-            modelInventario::changeUnits($id_inventory, $units_establishing);
 
+        if ($units_establishing !== null) {
+
+
+            modelInventario::changeUnits($id_inventory, $units_establishing);
         }
-        if(!empty($precio_costo)){
+        if (!empty($precio_costo)) {
 
             modelInventario::updatePrice($id_inventory, $precio_costo);
-
         }
 
-        if(!empty($name_inventory)){
+        if (!empty($name_inventory)) {
 
 
-           modelInventario::changeName($id_inventory, $name_inventory);
-           modelCompuesto::changeNameCompound($id_inventory, $name_inventory);
-
-
+            modelInventario::changeName($id_inventory, $name_inventory);
+            modelCompuesto::changeNameCompound($id_inventory, $name_inventory);
         }
 
 
-        if(!empty($unidades)){
+        if (!empty($unidades)) {
 
             $get_units = modelInventario::getUnits($id_inventory)->unidades_disponibles;
 
             $units_final = $get_units + $unidades;
-    
-            $insert_units = modelInventario::insertUnits($id_inventory, $units_final);
 
+            $insert_units = modelInventario::insertUnits($id_inventory, $units_final);
         }
 
 
@@ -108,73 +114,72 @@ class inventoryController extends Controller
     }
 
 
-    public function deleteInventory(Request $request){
+    public function deleteInventory(Request $request)
+    {
 
         $id_item = $request->id_item_inventory;
 
         $delete = modelInventario::deleteInventory($id_item);
         $delete_compund = modelCompuesto::deleteCompound($id_item);
 
-        if($delete && $delete_compund) return self::getShowInventory($request);
+        if ($delete && $delete_compund) return self::getShowInventory($request);
         else return response()->json(["status" => false]);
-
     }
 
 
-    private function calculateCosts($id_item_fk){
+    private function calculateCosts($id_item_fk)
+    {
 
         $composed = modelCompuesto::getIdProductSeller($id_item_fk);
-        
+
         $array_ids = [];
-        if($composed){
+        if ($composed) {
 
-            foreach($composed as $item){
+            foreach ($composed as $item) {
 
-                array_push($array_ids,[
+                array_push($array_ids, [
 
                     "id_producto_venta" => $item['id_producto_venta']
                 ]);
             }
+        } else return 0;
 
-        }else return 0;
 
-
-        foreach($array_ids as $item){
+        foreach ($array_ids as $item) {
 
 
             $verify_category = modelProducts::verifyCategory($item['id_producto_venta']);
 
-            if($verify_category->categoria == "tienda"){
-
-                return (!isset($verify_category->precio)) ? 0: $verify_category->precio;
+            if (isset($verify_category->categoria) && $verify_category->categoria == "tienda") {
+                return (!isset($verify_category->precio)) ? 0 : $verify_category->precio;
             }
-
         }
 
         return 0;
     }
 
 
-    private function comparativeInventory($date){
+    private function comparativeInventory($date)
+    {
 
         $verify_inventory = modelcontrol_inventarios::verifyExists($date);
         $array_data = [];
-        
-        if($verify_inventory){
+
+        if ($verify_inventory) {
 
             $original_inventory = modelInventario::getProductsCategory($this->category_store);
-            foreach($original_inventory as $item){
+            foreach ($original_inventory as $item) {
 
                 $id_item_original = $item['id_item'];
 
                 $row_product_control = modelcontrol_inventarios::getItemControl($id_item_original);
-                
-                $cantidad_control = (!isset($row_product_control->unidades_disponibles)) ? 0: $row_product_control->unidades_disponibles;
+
+                $cantidad_control = (!isset($row_product_control->unidades_disponibles)) ? 0 : $row_product_control->unidades_disponibles;
                 $restante = $item['unidades_disponibles'] - $cantidad_control;
-                
+
                 $calculate_cost = self::calculateCosts($id_item_original);
-                
-                array_push($array_data,[
+
+                array_push($array_data, [
 
                     "nombre" => $item['nombre'],
                     "item_original" => $item['unidades_disponibles'],
@@ -185,47 +190,44 @@ class inventoryController extends Controller
                     "fecha_reporte" => $row_product_control->fecha_reporte,
                     "total_faltante" => $restante * $calculate_cost
                 ]);
-
-                
             }
-
         }
 
-        
-        return $array_data;
 
+        return $array_data;
     }
 
-    private function verifyButton(){
+    private function verifyButton()
+    {
 
 
         $verify = modelcontrol_inventarios::verifyExists(date("Y-m-d"));
 
 
         return $verify;
-
     }
 
 
-    public function insertIventoryControl(Request $request){
+    public function insertIventoryControl(Request $request)
+    {
 
         $array_data = $request->array_data;
         $verification_button = self::verifyButton();
 
-        if($verification_button) return response()->json(["status" => false]);
+        if ($verification_button) return response()->json(["status" => false]);
 
         $flagg = 0;
-        foreach($array_data as $item){
+        foreach ($array_data as $item) {
 
             $id_item = $item['id_product_original'];
-            $cantidad = $item['count']; 
+            $cantidad = $item['count'];
 
             $row = modelInventario::verifyExists($id_item);
 
             $data_insert = [
 
                 "id_original" => $id_item,
-                "nombre" => $row->nombre ,
+                "nombre" => $row->nombre,
                 "unidades_disponibles" => $cantidad,
                 "fecha_reporte" => date("Y-m-d"),
                 "hora_reporte" => date('h:i:s'),
@@ -241,7 +243,5 @@ class inventoryController extends Controller
         $finally_results = ($flagg == count($array_data)) ? true : false;
 
         return response()->json(["status" => $finally_results]);
-
-
     }
 }
