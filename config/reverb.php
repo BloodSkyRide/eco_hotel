f701717c49a5ea02190fd8aea29d3cde268437f6
santiago <santiago.gallego1@utp.edu.co>
2025-09-20 15:16:51 -0500

    holandita

--- config/reverb.php
+++ config/reverb.php
@@ -29,9 +29,9 @@ return [
     'servers' => [
 
         'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
+            'host' => env('REVERB_SERVER_HOST', '127.0.0.1'),
             'port' => env('REVERB_SERVER_PORT', 6001),
-            'hostname' => env('REVERB_HOST',  '0.0.0.0'),
+            'hostname' => env('REVERB_HOST',  '127.0.0.1'),
             'options' => [
                 'tls' => [],
             ],
