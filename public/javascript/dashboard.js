c73cc9e572594b9bef6d3d3939f99f603d478b7a
santiago <santiago.gallego1@utp.edu.co>
2025-09-21 16:07:37 -0500

    hoy

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -3,7 +3,7 @@ var echo = new Echo({
     key: "7lznea8sbpv6xz0c3aqk",
     cluster: "mt1",
     wsHost: "intranetparadorrapi.com",
-    wsPath: "/app",
+    wsPath: "/",
     wsPort: 443,
     forceTLS: true,
     enabledTransports: ["ws","wss"],
